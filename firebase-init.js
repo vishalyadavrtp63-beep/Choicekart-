f2fccdcee5403940a01925719ecd8b8a651bd0f2
vishalyadavrtp63-beep <vishalyadav.rtp63@gmail.com>
2026-08-24 08:28:13 +0530

    Initialize Firebase app with authentication and storage

---
+++ firebase-init.js
@@ -0,0 +1,16 @@
+// Firebase Initialization
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
