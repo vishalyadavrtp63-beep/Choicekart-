51bfda4b180bbe47a6a5227be7bc09285af88741
vishalyadavrtp63-beep <vishalyadav.rtp63@gmail.com>
2026-08-24 08:27:11 +0530

    Create firebase-config.js with initial setup
    Add Firebase configuration template for ChoiceKart project

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// ChoiceKart Firebase Configuration
+// Firebase project banane ke baad yahan actual config add hoga
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Firebase config will be initialized later
